bb6dc9f5898ca8e7a544afb5d177889eaefd7c5d
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-26 16:56:12 -0300

    Refactorizar peliculas

--- sitio_peliculas/src/componentes/Resultado.jsx
+++ sitio_peliculas/src/componentes/Resultado.jsx
@@ -1,32 +1,32 @@
 import React, {Component} from 'react';
-import Imagen from "./Imagen";
+import Pelicula from "./Pelicula";
 import Paginacion from "./Paginacion";
 
 class Resultado extends Component{
 
     mostrarImagenes=()=>{
         
-        const imagenes= this.props.imagenes;
-        if(imagenes.length===0){
+        const peliculas= this.props.peliculas;
+        if(peliculas.length===0){
             return null;
         } 
         console.log("IMAGENES")
-        console.log(imagenes);
+        console.log(peliculas);
 
         return (
             <React.Fragment>
                 <div className="col-s12 p-5 row">
                     {
-                        imagenes.map((imagen, i)=>(
-                            <Imagen
+                        peliculas.map((pelicula, i)=>(
+                            <Pelicula
                             key={i}
-                            imagen={imagen.poster_path}
-                            titulo={imagen.title}
-                            estreno={imagen.release_date}
-                            lenguaje={imagen.original_language}
-                            descripcion={imagen.overview}
-                            popularidad={imagen.popularity}
-                            puntuacion={imagen.vote_average}
+                            imagen={pelicula.poster_path}
+                            titulo={pelicula.title}
+                            estreno={pelicula.release_date}
+                            lenguaje={pelicula.original_language}
+                            descripcion={pelicula.overview}
+                            popularidad={pelicula.popularity}
+                            puntuacion={pelicula.vote_average}
                             />
                         ))
                     }

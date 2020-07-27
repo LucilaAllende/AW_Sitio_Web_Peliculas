a66f61b257c60c6fc60e8d334aa1b2cfd956924c
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-27 17:30:19 -0300

    Mejorar paginacion

--- sitio_peliculas/src/componentes/Paginacion.jsx
+++ sitio_peliculas/src/componentes/Paginacion.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
 
 const Paginacion = (props) => {
     const linksPaginas = []
@@ -16,19 +26,44 @@ const Paginacion = (props) => {
     // Se crean los elementos de cada pagina.
     for( let i=1 ; i<=cantidadPaginas ; i++ ){
         let active = props.paginaActual === i ? 'active' : '';
-        linksPaginas.push(<li className={`waves-effect elemento-lista-paginacion ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="/#">{i}</a></li>)
+        linksPaginas.push(<li 
+                                className={`waves-effect elemento-lista-paginacion ${active}`} 
+                                key={i} 
+                                onClick={() => props.nextPage(i)}>
+                                    <a className="btn mr-1" href="/#">{i}</a>
+                            </li>)
     }
 
+    const classes = useStyles();
+
     return(
+        <> 
         <div className="container" >
             <div className="row">
                 <ul className="pagination">
-                    {props.paginaActual > 1 ? <li className="waves-effect elemento-lista-paginacion-prev"   onClick={() => props.nextPage(props.paginaActual - 1)}><a href="/#">Anterior</a></li> : ''}
-                        {linksPaginas}
-                    {props.paginaActual < props.paginas + 1 ? <li className="waves-effect elemento-lista-paginacion-next" onClick={() => props.nextPage(props.paginaActual + 1)}><a href="/#">Siguiente</a></li> : ''}
+                    {props.paginaActual > 1 ? 
+                        <li 
+                            className="waves-effect elemento-lista-paginacion-prev" 
+                            onClick={() => props.nextPage(props.paginaActual - 1)}>
+                                <a className="btn" href="/#">Anterior</a>
+                        </li> : ''
+                    }
+                    {linksPaginas}
+                    {props.paginaActual < props.paginas + 1 ? 
+                        <li 
+                            className="waves-effect elemento-lista-paginacion-next" 
+                            onClick={() => props.nextPage(props.paginaActual + 1)}>
+                                <a className="btn" href="/#">Siguiente</a>
+                        </li> : ''
+                    }
                 </ul>
             </div>
         </div>
+
+        <div className={classes.root}>
+            <Pagination count={10} />
+        </div>
+        </>
     )
 }
 

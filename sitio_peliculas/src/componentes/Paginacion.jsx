b32c2d3b9571251a4d1b5854818a26c47e0ca281
matias <matias.ducid08@gmail.com>
2020-07-27 01:36:53 -0300

    Limitar paginacion.

--- sitio_peliculas/src/componentes/Paginacion.jsx
+++ sitio_peliculas/src/componentes/Paginacion.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
 const Paginacion = (props) => {
     const linksPaginas = []
     
-    for( let i=1 ; i<=props.paginas +1 ; i++ ){
-        let active = props.paginaActual == i ? 'active' : '';
+    let varCantidadPaginas = 0
+    let varCantidadPaginasRestantes = 0
 
-    linksPaginas.push(<li className={`waves-effect elemento-lista-paginacion ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#">{i}</a></li>)
+    if (props.paginas >= 50){
+        varCantidadPaginas = 50
+        //varCantidadPaginasRestantes = props.paginas + 1 - varCantidadPaginas
+    }else{
+        varCantidadPaginas = props.paginas + 1
+    }
+    const cantidadPaginas = varCantidadPaginas
+    // Se crean los elementos de cada pagina.
+    for( let i=1 ; i<=cantidadPaginas ; i++ ){
+        let active = props.paginaActual == i ? 'active' : '';
+        linksPaginas.push(<li className={`waves-effect elemento-lista-paginacion ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#">{i}</a></li>)
     }
 
     return(
         <div className="container" >
             <div className="row">
                 <ul className="pagination">
-                    {props.paginaActual > 1 ? <li className="waves-effect elemento-lista-paginacion-prev"   onClick={() => props.nextPage(props.paginaActual - 1)}><a href="#">Prev</a></li> : ''}
+                    {props.paginaActual > 1 ? <li className="waves-effect elemento-lista-paginacion-prev"   onClick={() => props.nextPage(props.paginaActual - 1)}><a href="#">Anterior</a></li> : ''}
                         {linksPaginas}
-                    {props.paginaActual < props.paginas + 1 ? <li className="waves-effect elemento-lista-paginacion-next" onClick={() => props.nextPage(props.paginaActual + 1)}><a href="#">Next</a></li> : ''}
+                    {props.paginaActual < props.paginas + 1 ? <li className="waves-effect elemento-lista-paginacion-next" onClick={() => props.nextPage(props.paginaActual + 1)}><a href="#">Siguiente</a></li> : ''}
                 </ul>
             </div>
         </div>

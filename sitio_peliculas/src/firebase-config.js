303fc29216ed7691d3cb5d5ac5bd22c63ae5026f
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-28 11:07:37 -0300

    Inicializar app con firebase

--- sitio_peliculas/src/firebase-config.js
+++ sitio_peliculas/src/firebase-config.js
@@ -1,10 +1,21 @@
-export default {
-    apiKey: "AIzaSyAjafn7mXYw_W6PlLctj0cNbu3VVN9gJj0",
-    authDomain: "reactpeliculas.firebaseapp.com",
-    databaseURL: "https://reactpeliculas.firebaseio.com",
-    projectId: "reactpeliculas",
-    storageBucket: "reactpeliculas.appspot.com",
-    messagingSenderId: "412167875869",
-    appId: "1:412167875869:web:bc188fe122b53b8e3985f4",
-    measurementId: "G-HFBJER9L5C"
-  };
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+ var firebaseConfig = {
+  apiKey: "AIzaSyAjafn7mXYw_W6PlLctj0cNbu3VVN9gJj0",
+  authDomain: "reactpeliculas.firebaseapp.com",
+  databaseURL: "https://reactpeliculas.firebaseio.com",
+  projectId: "reactpeliculas",
+  storageBucket: "reactpeliculas.appspot.com",
+  messagingSenderId: "412167875869",
+  appId: "1:412167875869:web:bc188fe122b53b8e3985f4",
+  measurementId: "G-HFBJER9L5C"
+}
+
+// Initialize Firebase
+const fireb = firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+//var database = firebase.database(); 
+
+export default fireb;
